92ff7a10fdd7797105e74838fd76762a16889003
KokuKUSIAKU <kusiaku.k@gmail.com>
2017-09-25 15:23:19 +0100

    add player code

--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,46 @@
 
 ***************************************/
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template:__dirname+'/index.html',
-  filename:'index.html',
-  inject:'body',
+  template: __dirname + "/index.html",
+  filename: "index.html",
+  inject: "body",
 });
 
-module.exports ={
-  entry:[
-    './index.js',
+module.exports = {
+  entry: [
+    "babel-polyfill",
+    "./index.js",
   ],
   module: {
     /* to be changed to bale later */
-  /*loaders: [
-    {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-    {test: /\.css$/, exclude: /node_modules/, use: [ 'style-loader', 'css-loader' ]},
+    /*loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      {test: /\.css$/, exclude: /node_modules/, use: [ "style-loader", "css-loader" ]},
+  
+    ]*/
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
 
-  ]*/
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    },
-   {
-     test: /\.css$/,
-     use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/docs"
+  },
 
-       {loader:'style-loader'},
-       {loader:'css-loader'},
-     ]
-   }
- ]
-},
-output: {
-  filename: "bundle.js",
-  path: __dirname + '/docs'
-},
+  plugins: [HtmlWebpackPluginConfig]
 
-plugins:[HtmlWebpackPluginConfig]
-
-}
+};
